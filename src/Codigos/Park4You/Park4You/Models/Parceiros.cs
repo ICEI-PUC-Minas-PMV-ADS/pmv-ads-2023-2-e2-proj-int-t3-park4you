5747aae5249b851ed56c9629d46d01c0a3e14dbb
RickyPaulo <126209236+rickypaulo@users.noreply.github.com>
2023-09-30 21:04:26 +1000

    Atualização Classe Parceiro

--- src/Codigos/Park4You/Park4You/Models/Parceiros.cs
+++ src/Codigos/Park4You/Park4You/Models/Parceiros.cs
@@ -1,6 +1,19 @@
-﻿namespace Park4You.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Park4You.Models
 {
-    public class Parceiros
+    [Table("Parceiros")]
+    public class Parceiro
     {
+        [Key]
+        private int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório Informar o nome")]
+        private string nomeParceiro { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório Informar os Dados Bancários")]
+        private int dadosBancario { get; set; }
+
     }
 }
