c7a4933ee3ba81f383263eb5b4a34a7883a6d807
Alessandro Nunes Assuncao <nunes.1370451@gmail.com>
2023-10-19 23:18:01 -0300

    ok

---
+++ src/Codigos/Park4You/Park4You/Migrations/20231020012950_M06-AddTableVagas.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Park4You.Migrations
+{
+    public partial class M06AddTableVagas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
