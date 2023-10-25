1c0ac54d1bf890274b5a3fece3457b15d916a077
Paulo Carvalho <rickypaulo@hotmail.com>
2023-10-25 17:35:10 +1100

    Implementação API CEP no Criar e Editar Vagas

---
+++ src/projet_dev_backend/Views/Shared/_ConsultaCEP.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Projeto_Park4You.Views.Shared
+{
+    public class _ConsultaCEPModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
